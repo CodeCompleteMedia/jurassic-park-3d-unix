e6b17cf0db3409f220188d7904a859bcd8b9376e
ccm <matt@codecompletemedia.com>
2026-02-05 17:14:16 -0800

    Set folder.maxDistance=20 for zoom, folder.initialDistance=70 only on load

--- src/camera-config.js
+++ src/camera-config.js
@@ -5,8 +5,8 @@ export const CAMERA_CONFIG = {
   // Folder mode (viewing the entire folder platform)
   folder: {
     minDistance: 25,        // Closest zoom when viewing folder
-    maxDistance: 70,        // Furthest zoom when viewing folder (see multiple rows)
-    initialDistance: 70,    // Starting distance on load
+    maxDistance: 20,        // Furthest zoom when viewing folder (see multiple rows)
+    initialDistance: 70,    // Starting distance on load (only used once)
     minHeight: 10,          // Height at closest zoom
     maxHeight: 18,          // Height at furthest zoom
     initialHeight: 18       // Starting height
@@ -15,7 +15,7 @@ export const CAMERA_CONFIG = {
   // Node mode (viewing a selected node up close)
   node: {
     minDistance: 8,         // Closest zoom when viewing a node
-    maxDistance: 80,         // Furthest zoom when viewing a node
+    maxDistance: 25,         // Furthest zoom when viewing a node
     selectDistance: 8,       // Distance when first selecting a node
     minHeight: 6,           // Height at closest zoom
     maxHeight: 16           // Height at furthest zoom
